4d8d5db75ddecc9e16f8e1a46e41de210b71efef
JosenZQ <josenzamoraquesada02@gmail.com>
2022-12-06 10:48:55 -0600

    Creación de la Tabla de Medidores (Completa)

--- app/src/main/java/com/example/pcm_jzq_bqz/Clases/cMedidor.java
+++ app/src/main/java/com/example/pcm_jzq_bqz/Clases/cMedidor.java
@@ -19,10 +19,59 @@ public class cMedidor extends RealmObject {
         return NombreCliente;
     }
 
+    public int getCodigo() {
+        return Codigo;
+    }
 
+    public void setCodigo(int codigo) {
+        Codigo = codigo;
+    }
 
+    public int getCodigoSector() {
+        return CodigoSector;
+    }
 
+    public void setCodigoSector(int codigoSector) {
+        CodigoSector = codigoSector;
+    }
 
+    public int getSecuencia() {
+        return Secuencia;
+    }
 
+    public void setSecuencia(int secuencia) {
+        Secuencia = secuencia;
+    }
 
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String fecha) {
+        Fecha = fecha;
+    }
+
+    public String getNombreCliente() {
+        return NombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        NombreCliente = nombreCliente;
+    }
+
+    public String getLectura() {
+        return Lectura;
+    }
+
+    public void setLectura(String lectura) {
+        Lectura = lectura;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
 }

b5c560c3e0a40cac1b44ca33ac84c179f02a5540
JosenZQ <josenzamoraquesada02@gmail.com>
2022-12-06 15:33:54 -0600

    Cambios en el SectoresActivity

--- app/src/main/java/com/example/pcm_jzq_bqz/SectoresActivity.java
+++ app/src/main/java/com/example/pcm_jzq_bqz/SectoresActivity.java
@@ -5,24 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import com.example.pcm_jzq_bqz.Clases.cSector;
-import com.example.pcm_jzq_bqz.Clases.cSectorAdaptador;
-import com.example.pcm_jzq_bqz.Clases.cSectorServicio;
-
-import java.util.List;
-
-import io.realm.Realm;
 
 public class SectoresActivity extends AppCompatActivity {
 
-    cSectorServicio mServicio = new cSectorServicio(Realm.getDefaultInstance());
-    List<cSector> mListaSectores;
     ListView mlstListaSectores;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +17,6 @@ public class SectoresActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sectores);
 
         mlstListaSectores = findViewById(R.id.lstListaSectores);
-
-        fn_CargarListaSectores();
-        fn_CargarSectorSeleccionado();
-
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        fn_CargarListaSectores();
     }
 
     public void fn_Regresar(View view)
@@ -48,45 +24,10 @@ public class SectoresActivity extends AppCompatActivity {
         this.finish();
     }
 
-    public void fn_NuevoSector(View view)
+    public void fn_CargarNuevoSectorActivity(View view)
     {
-        Intent mPantalla = new Intent(this,NuevoSectorActivity.class);
+        Intent mPantalla = new Intent(SectoresActivity.this,NuevoSectorActivity.class);
         startActivity(mPantalla);
     }
 
-    private void fn_CargarListaSectores()
-    {
-        mListaSectores = mServicio.fn_ListaSectores();
-        cSectorAdaptador mSectorAdaptador = new cSectorAdaptador(this,mListaSectores,R.layout.adaptador_sectores);
-        mlstListaSectores.setAdapter(mSectorAdaptador);
-    }
-    //-----------------------------------
-    private void fn_CargarSectorSeleccionado()
-    {
-        mlstListaSectores.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int posicion, long l) {
-                cSector mSector = new cSector();
-                mSector = mServicio.fn_BuscarSectorPorCodigo(posicion+1);
-                String mMensaje = "Elemento seleccionado: " + mSector.getNombre();
-                Toast.makeText(SectoresActivity.this, mMensaje, Toast.LENGTH_SHORT).show();
-
-                Intent mPantalla = null;
-                switch (posicion)
-                {
-                    case 0:
-                        mPantalla = new Intent(SectoresActivity.this,MedidoresActivity.class);
-                        break;
-                    default:
-                        mPantalla = new Intent(SectoresActivity.this,MedidoresActivity.class);
-                        break;
-                }
-                startActivity(mPantalla);
-            }
-        });
-    }
-
-
-
-    //
 }

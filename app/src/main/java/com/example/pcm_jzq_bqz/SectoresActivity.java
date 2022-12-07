6da8ec7baf61dc51080b1077f93bb827d616c720
JosenZQ <josenzamoraquesada02@gmail.com>
2022-12-06 18:16:05 -0600

    Problema en el SectoresActivity solucionado

--- app/src/main/java/com/example/pcm_jzq_bqz/SectoresActivity.java
+++ app/src/main/java/com/example/pcm_jzq_bqz/SectoresActivity.java
@@ -5,10 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.pcm_jzq_bqz.Clases.cSector;
+import com.example.pcm_jzq_bqz.Clases.cSectorAdaptador;
+import com.example.pcm_jzq_bqz.Clases.cSectorServicio;
+
+import java.util.List;
+
+import io.realm.Realm;
 
 public class SectoresActivity extends AppCompatActivity {
-
+    cSectorServicio mServicio = new cSectorServicio(Realm.getDefaultInstance());
+    List<cSector> mListaSectores;
     ListView mlstListaSectores;
 
     @Override
@@ -17,6 +28,23 @@ public class SectoresActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sectores);
 
         mlstListaSectores = findViewById(R.id.lstListaSectores);
+
+        fn_CargarListaSectores();
+        fn_CargarSectorSeleccionado();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        fn_CargarListaSectores();
+    }
+
+    private void fn_CargarListaSectores()
+    {
+        mListaSectores = mServicio.fn_ListaSectores();
+        cSectorAdaptador mSectorAdaptador = new cSectorAdaptador(this,mListaSectores,R.layout.adaptador_sectores);
+        mlstListaSectores.setAdapter(mSectorAdaptador);
     }
 
     public void fn_Regresar(View view)
@@ -28,6 +56,31 @@ public class SectoresActivity extends AppCompatActivity {
     {
         Intent mPantalla = new Intent(SectoresActivity.this,NuevoSectorActivity.class);
         startActivity(mPantalla);
+    }
+
+    private void fn_CargarSectorSeleccionado()
+    {
+        mlstListaSectores.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicion, long l) {
+                cSector mSector = new cSector();
+                mSector = mServicio.fn_BuscarSectorPorCodigo(posicion+1);
+                String mMensaje = "Elemento seleccionado: " + mSector.getNombre();
+                Toast.makeText(SectoresActivity.this, mMensaje, Toast.LENGTH_SHORT).show();
+
+                Intent mPantalla = null;
+                switch (posicion)
+                {
+                    case 0:
+                        mPantalla = new Intent(SectoresActivity.this,MedidoresActivity.class);
+                        break;
+                    default:
+                        mPantalla = new Intent(SectoresActivity.this,MedidoresActivity.class);
+                        break;
+                }
+                startActivity(mPantalla);
+            }
+        });
     }
 
 }
